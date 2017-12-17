0b91bf9f7d6c27e58bd4d72022bf49169f25652a
chingchai <chingchai.h@gmail.com>
2017-12-17 20:00:31 +0700

    add sql script

---
+++ all_scripts/update_dopa_code.sql
@@ -0,0 +1,246 @@
+-- UPDATE c02_province SET procode = prov_code;
+-- UPDATE c03_district SET procode = prov_code;
+-- UPDATE c03_district SET ampcode = concat (prov_code, amp_code);
+-- UPDATE c04_subdistrict SET procode = prov_code;
+-- UPDATE c04_subdistrict SET ampcode = concat (prov_code, amp_code);
+-- UPDATE c04_subdistrict SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c05_municipa SET procode = prov_code;
+UPDATE c05_municipa SET ampcode = concat (prov_code, amp_code);
+UPDATE c05_municipa SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c06_village SET procode = prov_code;
+UPDATE c06_village SET ampcode = concat (prov_code, amp_code);
+UPDATE c06_village SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c07_ngo_prj SET procode = prov_code;
+UPDATE c07_ngo_prj SET ampcode = concat (prov_code, amp_code);
+UPDATE c07_ngo_prj SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c08_eia_prj SET procode = prov_code;
+UPDATE c08_eia_prj SET ampcode = concat (prov_code, amp_code);
+UPDATE c08_eia_prj SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c09_urb_plan SET procode = prov_code;
+UPDATE c09_urb_plan SET ampcode = concat (prov_code, amp_code);
+UPDATE c09_urb_plan SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c10_pc_zone SET procode = prov_code;
+UPDATE c10_pc_zone SET ampcode = concat (prov_code, amp_code);
+UPDATE c10_pc_zone SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c11_weather SET procode = prov_code;
+UPDATE c11_weather SET ampcode = concat (prov_code, amp_code);
+UPDATE c11_weather SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c12_noise SET procode = prov_code;
+UPDATE c12_noise SET ampcode = concat (prov_code, amp_code);
+UPDATE c12_noise SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c13_airqlt SET procode = prov_code;
+UPDATE c13_airqlt SET ampcode = concat (prov_code, amp_code);
+UPDATE c13_airqlt SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c14_waterqlt SET procode = prov_code;
+UPDATE c14_waterqlt SET ampcode = concat (prov_code, amp_code);
+UPDATE c14_waterqlt SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c15_contour SET procode = prov_code;
+UPDATE c15_contour SET ampcode = concat (prov_code, amp_code);
+UPDATE c15_contour SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c17_basin SET procode = prov_code;
+UPDATE c17_basin SET ampcode = concat (prov_code, amp_code);
+UPDATE c17_basin SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c18_stream SET procode = prov_code;
+UPDATE c18_stream SET ampcode = concat (prov_code, amp_code);
+UPDATE c18_stream SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c19_natural_wtr_body SET procode = prov_code;
+UPDATE c19_natural_wtr_body SET ampcode = concat (prov_code, amp_code);
+UPDATE c19_natural_wtr_body SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c20_reservoir SET procode = prov_code;
+UPDATE c20_reservoir SET ampcode = concat (prov_code, amp_code);
+UPDATE c20_reservoir SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c21_strm_gage SET procode = prov_code;
+UPDATE c21_strm_gage SET ampcode = concat (prov_code, amp_code);
+UPDATE c21_strm_gage SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c22_wshd_cl SET procode = prov_code;
+UPDATE c22_wshd_cl SET ampcode = concat (prov_code, amp_code);
+UPDATE c22_wshd_cl SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c23_vil_wtrsupply SET procode = prov_code;
+UPDATE c23_vil_wtrsupply SET ampcode = concat (prov_code, amp_code);
+UPDATE c23_vil_wtrsupply SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c24_hydrogeology SET procode = prov_code;
+UPDATE c24_hydrogeology SET ampcode = concat (prov_code, amp_code);
+UPDATE c24_hydrogeology SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c25_well SET procode = prov_code;
+UPDATE c25_well SET ampcode = concat (prov_code, amp_code);
+UPDATE c25_well SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c26_geology SET procode = prov_code;
+UPDATE c26_geology SET ampcode = concat (prov_code, amp_code);
+UPDATE c26_geology SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c27_soil SET procode = prov_code;
+UPDATE c27_soil SET ampcode = concat (prov_code, amp_code);
+UPDATE c27_soil SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c28_foresttype SET procode = prov_code;
+UPDATE c28_foresttype SET ampcode = concat (prov_code, amp_code);
+UPDATE c28_foresttype SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c29_nrf SET procode = prov_code;
+UPDATE c29_nrf SET ampcode = concat (prov_code, amp_code);
+UPDATE c29_nrf SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c30_nprk	 SET procode = prov_code;
+UPDATE c30_nprk	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c30_nprk	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c31_fprk	 SET procode = prov_code;
+UPDATE c31_fprk	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c31_fprk	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c32_wldsshp	 SET procode = prov_code;
+UPDATE c32_wldsshp	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c32_wldsshp	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c33_nhw	 SET procode = prov_code;
+UPDATE c33_nhw	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c33_nhw	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c34_forestzoning	 SET procode = prov_code;
+UPDATE c34_forestzoning	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c34_forestzoning	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c35_alro	 SET procode = prov_code;
+UPDATE c35_alro	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c35_alro	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c36_landuse	 SET procode = prov_code;
+UPDATE c36_landuse	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c36_landuse	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c37_soilsuit	 SET procode = prov_code;
+UPDATE c37_soilsuit	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c37_soilsuit	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c38_heritage	 SET procode = prov_code;
+UPDATE c38_heritage	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c38_heritage	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c39_road	 SET procode = prov_code;
+UPDATE c39_road	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c39_road	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c40_rail	 SET procode = prov_code;
+UPDATE c40_rail	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c40_rail	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c41_harbour	 SET procode = prov_code;
+UPDATE c41_harbour	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c41_harbour	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c42_airport	 SET procode = prov_code;
+UPDATE c42_airport	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c42_airport	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c43_electr	 SET procode = prov_code;
+UPDATE c43_electr	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c43_electr	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c44_watersupply	 SET procode = prov_code;
+UPDATE c44_watersupply	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c44_watersupply	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c45_telephone	 SET procode = prov_code;
+UPDATE c45_telephone	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c45_telephone	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c46_factory	 SET procode = prov_code;
+UPDATE c46_factory	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c46_factory	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c47_hospital	 SET procode = prov_code;
+UPDATE c47_hospital	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c47_hospital	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c48_school	 SET procode = prov_code;
+UPDATE c48_school	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c48_school	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c49_religious	 SET procode = prov_code;
+UPDATE c49_religious	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c49_religious	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c50_policestation	 SET procode = prov_code;
+UPDATE c50_policestation	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c50_policestation	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c51_gasoline	 SET procode = prov_code;
+UPDATE c51_gasoline	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c51_gasoline	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c52_postoffice	 SET procode = prov_code;
+UPDATE c52_postoffice	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c52_postoffice	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c53_hazard	 SET procode = prov_code;
+UPDATE c53_hazard	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c53_hazard	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c54_flood	 SET procode = prov_code;
+UPDATE c54_flood	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c54_flood	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c55_earthquake	 SET procode = prov_code;
+UPDATE c55_earthquake	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c55_earthquake	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c56_landslide	 SET procode = prov_code;
+UPDATE c56_landslide	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c56_landslide	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c57_sinkhole	 SET procode = prov_code;
+UPDATE c57_sinkhole	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c57_sinkhole	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c58_drought	 SET procode = prov_code;
+UPDATE c58_drought	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c58_drought	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c59_point_source	 SET procode = prov_code;
+UPDATE c59_point_source	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c59_point_source	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c60_non_point_source	 SET procode = prov_code;
+UPDATE c60_non_point_source	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c60_non_point_source	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c61_rf30y	 SET procode = prov_code;
+UPDATE c61_rf30y	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c61_rf30y	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c62_temp30y	 SET procode = prov_code;
+UPDATE c62_temp30y	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c62_temp30y	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c63_encroachforest	 SET procode = prov_code;
+UPDATE c63_encroachforest	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c63_encroachforest	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c64_wetland	 SET procode = prov_code;
+UPDATE c64_wetland	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c64_wetland	 SET tamcode = concat (prov_code, amp_code, tam_code);
+
+UPDATE c65_saltearth	 SET procode = prov_code;
+UPDATE c65_saltearth	 SET ampcode = concat (prov_code, amp_code);
+UPDATE c65_saltearth	 SET tamcode = concat (prov_code, amp_code, tam_code);
